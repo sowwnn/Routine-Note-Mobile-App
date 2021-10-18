b621ca3f216769159f2e8fb83e604f35409e9042
ZaQuyn <tin1232020@gmail.com>
2021-10-18 21:07:01 +0700

    Update login, font

---
+++ Dreamroutine/app/src/main/java/com/example/dream_routine/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.dream_routine;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
