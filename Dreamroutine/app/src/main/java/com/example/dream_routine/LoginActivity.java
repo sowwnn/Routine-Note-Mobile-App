6bbec47f40790550b7ac36ee5533e8d8e11d9e2e
ZaQuyn <tin1232020@gmail.com>
2021-11-03 13:35:58 +0700

    add login & register intent

--- Dreamroutine/app/src/main/java/com/example/dream_routine/LoginActivity.java
+++ Dreamroutine/app/src/main/java/com/example/dream_routine/LoginActivity.java
@@ -7,16 +7,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
     Button btnLogin;
+    TextView txtRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
         btnLogin = findViewById(R.id.btnLogin);
-
+        txtRegister = findViewById(R.id.register);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,6 +27,14 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(login);
             }
         });
+        txtRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent register = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(register);
+            }
+        });
+
     }
 
 
--- Dreamroutine/app/src/main/java/com/example/dream_routine/RegisterActivity.java
+++ Dreamroutine/app/src/main/java/com/example/dream_routine/RegisterActivity.java
@@ -2,13 +2,26 @@ package com.example.dream_routine;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    TextView txtLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        txtLogin = findViewById(R.id.login);
+
+        txtLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent login = new Intent(RegisterActivity.this,LoginActivity.class);
+                startActivity(login);
+            }
+        });
     }
 }
