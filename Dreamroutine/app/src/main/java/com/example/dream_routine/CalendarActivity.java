d5133b42cf9a9d0f0710a542d78d89777b190771
RC-Sho0 <mac@MACs-MacBook-Pro.local>
2021-11-30 22:50:03 +0700

    Update Bottomsheet

--- Dreamroutine/app/src/main/java/com/example/dream_routine/CalendarActivity.java
+++ Dreamroutine/app/src/main/java/com/example/dream_routine/CalendarActivity.java
@@ -7,10 +7,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -28,6 +32,7 @@ public class CalendarActivity extends AppCompatActivity {
 
     //var
     TextView title;
+    ArrayList<String> drdtags = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,6 +66,11 @@ public class CalendarActivity extends AppCompatActivity {
 
 
         //BottomSheet
+
+        drdtags.add("Machine Learning");
+        drdtags.add("Android");
+        drdtags.add("Database Adv");
+
         btnnewtask = findViewById(R.id.btnnewtask);
         btnnewtask.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,11 +79,32 @@ public class CalendarActivity extends AppCompatActivity {
             }
         });
     }
-    private void showBottomSheetDialog(){
+    public void showBottomSheetDialog(){
         bottomSheetNewTask = new BottomSheetDialog(CalendarActivity.this, R.style.BottomSheetStyle);
         View v = LayoutInflater.from(CalendarActivity.this).inflate(R.layout.bottom_sheet_new_task,findViewById(R.id.bottomsheet));
         bottomSheetNewTask.setContentView(v);
 
+//        var
+        Button btnsave = bottomSheetNewTask.findViewById(R.id.btnsave);
+        EditText txttaskname = bottomSheetNewTask.findViewById(R.id.txttaskname);
+        EditText txtdeadline = bottomSheetNewTask.findViewById(R.id.txtdeadline);
+        Spinner sptag = bottomSheetNewTask.findViewById(R.id.sptag);
+        EditText txttag = bottomSheetNewTask.findViewById(R.id.txttag);
+        EditText txtnote = bottomSheetNewTask.findViewById(R.id.txtnote);
+
         bottomSheetNewTask.show();
+
+        sptag.setAdapter(new ArrayAdapter<String>(CalendarActivity.this, android.R.layout.simple_dropdown_item_1line, drdtags));
+        String tag = sptag.getSelectedItem().toString();
+
+        btnsave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"New task: "+ txttaskname.getText()+" \nin: "+tag+" \ndeadline: "+txtdeadline.getText(),Toast.LENGTH_LONG).show();
+                bottomSheetNewTask.dismiss();
+            }
+        });
+
     }
+
 }
